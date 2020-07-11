6da2219bb51ba2a010e1b5db3e0a57a3e8dbb185
matt-mekha <mtmekha8@gmail.com>
2020-07-11 15:16:35 -0500

    mouse highlights obstacles

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -17,14 +17,18 @@ public class GameScript : MonoBehaviour
     private const int obstaclesPerTileMax = 6;
     private const float obstacleXRange = 7f;
 
+    private const int obstacleLayer = 8;
+
 
 
     private GameObject playerPrefab;
     private GameObject obstacleWrapperPrefab;
+    private GameObject outlinePrefab;
     private List<List<GameObject>> groundTilePrefabs = new List<List<GameObject>>();
     private List<List<GameObject>> obstaclePrefabs = new List<List<GameObject>>();
 
     private GameObject player;
+    private new Camera camera;
     private List<GameObject> groundTiles = new List<GameObject>();
     private List<GameObject> obstacles = new List<GameObject>();
 
@@ -32,6 +36,9 @@ public class GameScript : MonoBehaviour
 
     private int nextTileCount;
     private float nextTileSpawnThreshold;
+
+    private GameObject hoveredObject;
+    private List<GameObject> outlines = new List<GameObject>();
 
 
     
@@ -44,6 +51,7 @@ public class GameScript : MonoBehaviour
         }
         playerPrefab = Resources.Load<GameObject>("Player");
         obstacleWrapperPrefab = Resources.Load<GameObject>("ObstacleWrapper");
+        outlinePrefab = Resources.Load<GameObject>("Outline");
     }
 
     void Start()
@@ -55,8 +63,10 @@ public class GameScript : MonoBehaviour
         distance = 0;
         nextTileCount = 0;
         nextTileSpawnThreshold = tileSize;
+        hoveredObject = null;
 
         player = Instantiate(playerPrefab);
+        camera = player.transform.Find("Camera").GetComponent<Camera>();
         for (int i = 0; i < numTilesAtOnce; i++)
         {
             SpawnNextTile();
@@ -69,6 +79,16 @@ public class GameScript : MonoBehaviour
         GameObject obstacleWrapper = Instantiate(obstacleWrapperPrefab, tile.transform);
         GameObject obstacle = Instantiate(obstaclePrefab, obstacleWrapper.transform);
         obstacleWrapper.transform.localPosition = new Vector3(Random.Range(-obstacleXRange, obstacleXRange), 0, Random.Range(-tileSize/2, tileSize/2)) / 10f;
+        
+        AddMeshCollider(obstacle);
+    }
+
+    private void AddMeshCollider(GameObject obj) {
+        obj.AddComponent<MeshCollider>();
+        obj.layer = obstacleLayer;
+        foreach(Transform child in obj.transform) {
+            AddMeshCollider(child.gameObject);
+        }
     }
 
     private void SpawnNextTile() {
@@ -106,9 +126,51 @@ public class GameScript : MonoBehaviour
             DespawnLastTile();
             SpawnNextTile();
         }
+
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << obstacleLayer)) {
+            hit.transform.SendMessageUpwards("OnHover", this);
+        } else {
+            OnHover(null);
+        }
+    }
+
+    public void OnHover(GameObject newHoveredObject) {
+        if(hoveredObject == newHoveredObject) return;
+
+        hoveredObject = newHoveredObject;
+
+        foreach(GameObject outline in outlines) {
+            Destroy(outline);
+        }
+        outlines.Clear();
+
+        if(hoveredObject != null) {
+            Outline(hoveredObject.transform);
+        }
+    }
+
+    private void Outline(Transform parent) {
+        if(parent.tag == "Outline") return;
+
+        MeshFilter meshFilter = parent.GetComponent<MeshFilter>();
+        if(meshFilter != null) {
+            GameObject outline = Instantiate(outlinePrefab, parent);
+            outline.GetComponent<MeshFilter>().mesh = meshFilter.mesh;
+            outlines.Add(outline);
+        }
+
+        foreach (Transform child in parent)
+        {
+            Outline(child);
+        }
     }
 
     public void EndGame() {
+        OnHover(null);
         Destroy(player);
         foreach (GameObject groundTile in groundTiles)
         {
---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    void OnHover(GameScript gameScript) {
+        gameScript.OnHover(gameObject);
+    }
+}
